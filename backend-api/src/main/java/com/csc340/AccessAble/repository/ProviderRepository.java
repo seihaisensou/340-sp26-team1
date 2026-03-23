41f750ec72d4fb2b75c0a6791011ea88e98d2720
Kendall Williford <KMWILLIFORD@uncg.edu>
2026-03-23 19:06:24 -0400

    Created provider repo and listing repo pages

--- backend-api/src/main/java/com/csc340/AccessAble/repository/ProviderRepository.java
+++ backend-api/src/main/java/com/csc340/AccessAble/repository/ProviderRepository.java
@@ -1,5 +1,8 @@
 package com.csc340.AccessAble.repository;
 
-public class ProviderRepository {
-    
+import com.csc340.AccessAble.entity.Provider;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProviderRepository extends JpaRepository<Provider, Long> {
+
 }
